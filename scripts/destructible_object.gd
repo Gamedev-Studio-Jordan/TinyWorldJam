b2b29489c3d9c4c0b787b17d2f2011ac69f7b595
OsamaHeweitat <osamaheweitatdev@gmail.com>
2025-09-10 15:22:30 +0300

    Added proper object test scene

--- scripts/destructible_object.gd
+++ scripts/destructible_object.gd
@@ -3,5 +3,5 @@ extends RigidBody3D
 @export var destruction_node: Destruction
 @export var explosion_power: float = 7
 
-func destroy_object() -> void:
-    destruction_node.destroy(explosion_power)
+func destroy_object(explosion_power_multiplier: float = 1.0) -> void:
+    destruction_node.destroy(explosion_power * explosion_power_multiplier)
