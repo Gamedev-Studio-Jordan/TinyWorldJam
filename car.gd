623d088a4ef70db19ae19e012e2235a92c428965
OsamaHeweitat <osamaheweitatdev@gmail.com>
2025-09-11 01:53:57 +0300

    Gravity overhaul

--- car.gd
+++ car.gd
@@ -15,13 +15,13 @@ class_name Car extends RigidBody3D
 @export_category("Debug")
 @export var debug_force: bool = false
 @export var debug_force_line: Line3D = null
+@export var debug_forward_line: Line3D = null
 
 var planet: Planet = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	car.top_level = true
-
 	planet = get_tree().get_first_node_in_group("planet")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,37 +33,31 @@ func _physics_process(delta: float) -> void:
 	
 	_curve_effect(delta)
 	
+	# Apply turning to the car visual
 	car.rotate_y(turn_degree)
-
-	var force: Vector3 = -car.global_transform.basis.z * speed_force
-
+	
 	if planet:
 		var gravity_centre: Vector3 = planet.get_gravity_centre()
 		var direction_to_planet: Vector3 = (gravity_centre - global_position).normalized()
 		
-		# This creates a rotation that aligns the car with the planet
-		var up_direction: Vector3 = direction_to_planet
-		var forward_direction: Vector3 = car.global_transform.basis.z
-		var right_direction: Vector3 = up_direction.cross(forward_direction).normalized()
-		var corrected_forward: Vector3 = right_direction.cross(up_direction).normalized()
+		# Align the car with the planet surface
+		var current_basis = car.global_transform.basis
+		var new_y = direction_to_planet
+		var new_z = current_basis.z - new_y * current_basis.z.dot(new_y)
+		new_z = new_z.normalized()
+		var new_x = new_y.cross(new_z).normalized()
 		
-		# Creates a new basis with the corrected directions
-		var surface_basis: Basis = Basis()
-		surface_basis.x = right_direction
-		surface_basis.y = up_direction
-		surface_basis.z = corrected_forward
+		# Create the new basis for the car
+		var surface_basis = Basis(new_x, new_y, new_z)
+		car.global_transform.basis = surface_basis
 		
-		if car.global_position.y - gravity_centre.y <= 0:
-			surface_basis.z = -surface_basis.z
-			
-		force = surface_basis * Vector3(0, 0, -speed_force)
-
-		car_mesh.global_transform.basis = Basis(surface_basis.x, -surface_basis.y, surface_basis.z)
-	
-	if debug_force:
-		draw_line(force)
-	
-	apply_force(force)
+		# Apply force in the car's forward direction (relative to the planet surface)
+		var force = -car.global_transform.basis.z * speed_force
+		
+		if debug_force:
+			draw_line(force, debug_force_line)
+		
+		apply_force(force)
 
 func _curve_effect(delta) -> void:
 	var turnStrengthValue = turn_degree * linear_velocity.length() / effect_turn_speed
@@ -75,7 +69,7 @@ func _curve_effect(delta) -> void:
 	car_mesh.rotation.y = lerp(car_mesh.rotation.y, turnStrengthValue, changeSpeed * delta)
 	car_mesh.rotation.z = lerp(car_mesh.rotation.z, turnTiltValue, changeSpeed * delta)
 
-func draw_line(force: Vector3) -> void:
-	if debug_force_line:
-		debug_force_line.set_start(global_position)
-		debug_force_line.set_end(global_position + force)
+func draw_line(force: Vector3, line: Line3D) -> void:
+	if line:
+		line.set_start(global_position)
+		line.set_end(global_position + force)
